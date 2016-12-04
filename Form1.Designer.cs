0022fc92fe6398670f1838da5364f48ac31893aa
ruslan79k <ruslan79k@gmail.com>
2016-12-04 16:02:10 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace EVE_SettingsMirror
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_User = new System.Windows.Forms.TextBox();
+            this.textBox_Char = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.donate_ISK = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(12, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Update all files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_User
+            // 
+            this.textBox_User.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_User.Location = new System.Drawing.Point(6, 80);
+            this.textBox_User.Name = "textBox_User";
+            this.textBox_User.Size = new System.Drawing.Size(174, 20);
+            this.textBox_User.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.textBox_User, "Example: core_user_15463258.dat");
+            this.textBox_User.TextChanged += new System.EventHandler(this.TextBoxTextChanged);
+            // 
+            // textBox_Char
+            // 
+            this.textBox_Char.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Char.Location = new System.Drawing.Point(6, 31);
+            this.textBox_Char.Name = "textBox_Char";
+            this.textBox_Char.Size = new System.Drawing.Size(174, 20);
+            this.textBox_Char.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.textBox_Char, "Example: core_char_5146325.dat");
+            this.textBox_Char.TextChanged += new System.EventHandler(this.TextBoxTextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_User);
+            this.groupBox1.Controls.Add(this.textBox_Char);
+            this.groupBox1.Location = new System.Drawing.Point(12, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(186, 118);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sample";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "core_char_***.dat";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "core_user_***.dat";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 100;
+            this.toolTip1.AutoPopDelay = 10000;
+            this.toolTip1.InitialDelay = 10;
+            this.toolTip1.ReshowDelay = 20;
+            // 
+            // listBoxLog
+            // 
+            this.listBoxLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.Location = new System.Drawing.Point(3, 16);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(180, 92);
+            this.listBoxLog.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.listBoxLog);
+            this.groupBox2.Location = new System.Drawing.Point(12, 235);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(186, 111);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Updated files:";
+            // 
+            // donate_ISK
+            // 
+            this.donate_ISK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.donate_ISK.AutoSize = true;
+            this.donate_ISK.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.donate_ISK.ForeColor = System.Drawing.Color.DarkRed;
+            this.donate_ISK.Location = new System.Drawing.Point(136, 9);
+            this.donate_ISK.Name = "donate_ISK";
+            this.donate_ISK.Size = new System.Drawing.Size(62, 13);
+            this.donate_ISK.TabIndex = 6;
+            this.donate_ISK.Text = "Donate ISK";
+            this.donate_ISK.Click += new System.EventHandler(this.donate_ISK_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 348);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.donate_ISK);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EVE Settings Mirror";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Save);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_User;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBox_Char;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxLog;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label donate_ISK;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace EVE_SettingsMirror
+{
+    public partial class Form1 : Form
+    {
+        readonly XElement _xElement;
+        private readonly string[] _files;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                _xElement = XElement.Load("EVE_SettingsMirror.xml");
+                var xElement = _xElement.Element("SampleUser");
+                if (xElement != null)
+                {
+                    textBox_User.Text = xElement.Value;
+                    if (xElement.Value != string.Empty && !File.Exists(xElement.Value)) textBox_User.Text = string.Empty;
+                }
+
+                xElement = _xElement.Element("SampleChar");
+                if (xElement != null)
+                {
+                    textBox_Char.Text = xElement.Value;
+                    if (xElement.Value != string.Empty && !File.Exists(xElement.Value)) textBox_Char.Text = string.Empty;
+                }
+            }
+            catch (Exception)
+            {
+                _xElement = new XElement("r");
+            }
+
+
+            #region чтение папок настроек игры и вывод в меню
+
+            string pathEve = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                @"CCP\EVE\");
+            var f = Directory.GetFiles(pathEve, "core_public__.yaml", SearchOption.AllDirectories);
+            foreach (var path in f)
+            {
+                var folder = Path.GetDirectoryName(path);
+                if (folder != null)
+                {
+                    var directoryInfo = new DirectoryInfo(folder);
+                    ToolStripItem item = new ToolStripMenuItem("Open folder: " + directoryInfo.Name);
+                    item.Tag = folder;
+                    contextMenuStrip1.Items.Add(item);
+                }
+            }
+
+            #endregion
+
+            _files = Directory.GetFiles(Application.StartupPath, "*.dat", SearchOption.TopDirectoryOnly);
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBoxLog.Items.Clear();
+
+            foreach (string fPath in _files)
+            {
+                string fileName = Path.GetFileName(fPath);
+
+                if (fileName == textBox_Char.Text || fileName == textBox_User.Text) continue;
+
+                if (fileName != null)
+                {
+                    Regex regex = new Regex(@"(user_|char_)(\d+)");
+                    Match match = regex.Match(fileName);
+                    while (match.Success)
+                    {
+                        if (textBox_User.Text != string.Empty && match.Groups[1].Value == "user_")
+                        {
+                            File.Copy(Path.Combine(Application.StartupPath, textBox_User.Text), fPath, true);
+                            listBoxLog.Items.Add(fileName);
+                        }
+                        if (textBox_Char.Text != string.Empty && match.Groups[1].Value == "char_")
+                        {
+                            File.Copy(Path.Combine(Application.StartupPath, textBox_Char.Text), fPath, true);
+                            listBoxLog.Items.Add(fileName);
+                        }
+
+                        match = match.NextMatch();
+                    }
+                }
+            }
+        }
+
+        private void Save(object sender, FormClosingEventArgs e)
+        {
+            if (textBox_Char.Text != string.Empty || textBox_User.Text != string.Empty)
+            {
+                _xElement.SetElementValue("SampleUser", textBox_User.Text);
+                _xElement.SetElementValue("SampleChar", textBox_Char.Text);
+                _xElement.Save("EVE_SettingsMirror.xml");
+            }
+        }
+
+        private void TextBoxTextChanged(object sender, EventArgs e)
+        {
+            var tb = (TextBox)sender;
+            tb.Text = tb.Text.Trim().Replace(" ", string.Empty);
+        }
+
+        private void donate_ISK_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Donate ISK in the game can be addressed to: x989\n" +
+                             "Пожертвовать ISK можно в игре на имя: x989");
+        }
+
+
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            Process.Start(e.ClickedItem.Tag.ToString());
+        }
+
+      
+    }
+}
